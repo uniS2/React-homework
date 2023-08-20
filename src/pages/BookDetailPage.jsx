7699c98f5c58ca205437399fc210af28cb1bc752
uniS2 <s2oie1010@gmail.com>
2023-08-21 05:29:47 +0900

    add. page 구성 - 책 상세 정보페이지

---
+++ src/pages/BookDetailPage.jsx
@@ -0,0 +1,32 @@
+import useFetchData from "../hooks/useFecthData";
+import Spinner from "../components/Spinner";
+import BookProfile from "../layout/BookProfile";
+import BookHome from "../layout/BookHome";
+import BookIntroduction from "../layout/BookIntroduction";
+
+function BookDetailPage() {
+  const { isLoading, error } = useFetchData();
+
+  if (isLoading) {
+    return <Spinner size={160} />;
+  }
+
+  if (error && !(error instanceof DOMException)) {
+    return (
+      <div role="alert">
+        <h2>{error.type}</h2>
+        <p>{error.message}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex justify-center">
+      <BookProfile />
+      {/* <BookHome /> */}
+      <BookIntroduction />
+    </div>
+  );
+}
+
+export default BookDetailPage;
